6c22ed512e798e2d96126984f9a4723d296805b5
Rafael Amaro Rolfsen <rafael.rolfsen@usp.br>
2019-11-28 20:46:27 -0300

    Add comments, and states names

--- main.js
+++ main.js
@@ -59,18 +59,25 @@ const csv = d3.csv("amazon.csv", function (data){
 
 })
 
+// variaveis para controle do tamanho das barras
+let larguraBarra = 30;
+let alturaBarra = 150;
+let espacoBarra = 35;
+
 function criarANO(){
 
+    // Adiciona o título da tabela
     d3.select("body").append("h2").text("Queimadas por ano (em milhares)")
-                .attr("x", porAno.length*30/2)
-                .style("color", "black")
+                .style("color", "black")                    
                 .style("text-align","center");
 
+    // Cria espaço onde será gerado o gráfico
     var canvas = d3.select("body").append("svg")
                 .attr("id", "grafico1")
-                .attr("width", porAno.length*35)
-                .attr("height", 200)
+                .attr("width", porAno.length * espacoBarra)
+                .attr("height", alturaBarra+50)
 
+    // Pega maior e menor valor do vetor
     var maior = 0;
     var menor = Number.MAX_SAFE_INTEGER;
     porAno.forEach((element) =>  {
@@ -81,44 +88,52 @@ function criarANO(){
             menor = element.number;
         }
     });
-    if(menor != 0){
+
+    // aumenta o valor das barras de forma igualitaria para que a menor não fique com tamanho 0.
+    if(menor != 0){  
         menor = menor - maior/10;
     }
 
+    // Gera barras para cada elemento por aglomerado por ano
     for(let i = 0; i < porAno.length; i++){
-        var aux = (porAno[i].number-menor)/(maior-menor);
-        canvas.append("rect")
-            .attr("width", 30)
-            .attr("height", aux*100)
-            .attr("fill", "blue")
-            .attr("x", i*35)
-            .attr("y", 150 - aux*100)
+
+        // normaliza os valores
+        var aux = (porAno[i].number-menor)/(maior-menor); // normaliza os valores
+
+        // adiciona novo container para cada barra e preenche os dados de cada uma
+        canvas.append("rect")               
+            .attr("width", larguraBarra)    
+            .attr("height", aux*100)  
+            .attr("fill", "blue")     
+            .attr("x", i * espacoBarra)
+            .attr("y", alturaBarra - aux*100)
             .on("mouseover", function(){
                 d3.select(this)
                 .style("fill", "red");
                 d3.select("#labelano"+i)
                 .style("visibility", "visible")
-            })
+            })                          
             .on("mouseout", function(){
                 d3.select(this)
                 .style("fill", "blue");
                 d3.select("#labelano"+i)
                 .style("visibility", "hidden")
             })
+
+        // Adiciona o label inferior de cada barra
         canvas.append("text")
-                .attr("width", 30)
+                .attr("width", larguraBarra)
                 .attr("height", 10)
-                .attr("x", i*35)
-                .attr("y", 165)
+                .attr("x", i * espacoBarra)
+                .attr("y", alturaBarra + 15)
                 .attr("fill", "black")
                 .text( porAno[i].year )
         
+        // Adiciona o valor numerico de cada barra
         canvas.append("text")
                 .attr("id", "labelano" + i)
-                .attr("width", 30)
-                .attr("height", 10)
-                .attr("x", i*35)
-                .attr("y", 150-aux*100-2)
+                .attr("x", i * espacoBarra)
+                .attr("y", alturaBarra - aux * 100 - 2)
                 .attr("fill", "black")
                 .text(  Math.floor(porAno[i].number/1000) )
                 .style("visibility", "hidden")
@@ -127,16 +142,18 @@ function criarANO(){
 
 function criarEstado(){
 
+    // Adiciona o título da tabela
     d3.select("body").append("h2").text("Queimadas por estado (em milhares)")
-                .attr("x", porEstado.length*30/2)
-                .style("color", "black")
+                .style("color", "black")                    
                 .style("text-align","center");
 
+    // Cria espaço onde será gerado o gráfico
     var canvas = d3.select("body").append("svg")
                 .attr("id", "grafico2")
-                .attr("width", porEstado.length*35)
-                .attr("height", 200)
+                .attr("width", porEstado.length * espacoBarra)
+                .attr("height", alturaBarra+50)
 
+    // Pega maior e menor valor do vetor
     var maior = 0;
     var menor = Number.MAX_SAFE_INTEGER;
     porEstado.forEach((element) =>  {
@@ -147,44 +164,52 @@ function criarEstado(){
             menor = element.number;
         }
     });
-    if(menor != 0){
+
+    // aumenta o valor das barras de forma igualitaria para que a menor não fique com tamanho 0.
+    if(menor != 0){  
         menor = menor - maior/10;
     }
 
+    // Gera barras para cada elemento por aglomerado por estado
     for(let i = 0; i < porEstado.length; i++){
-        var aux = (porEstado[i].number-menor)/(maior-menor);
-        canvas.append("rect")
-            .attr("width", 30)
-            .attr("height", aux*100)
-            .attr("fill", "blue")
-            .attr("x", i*35)
-            .attr("y", 150 - aux*100)
+
+        // normaliza os valores
+        var aux = (porEstado[i].number-menor)/(maior-menor); // normaliza os valores
+
+        // adiciona novo container para cada barra e preenche os dados de cada uma
+        canvas.append("rect")               
+            .attr("width", larguraBarra)    
+            .attr("height", aux*100)  
+            .attr("fill", "blue")     
+            .attr("x", i * espacoBarra)
+            .attr("y", alturaBarra - aux*100)
             .on("mouseover", function(){
                 d3.select(this)
                 .style("fill", "red");
                 d3.select("#labelestado"+i)
                 .style("visibility", "visible")
-            })
+            })                          
             .on("mouseout", function(){
                 d3.select(this)
                 .style("fill", "blue");
                 d3.select("#labelestado"+i)
                 .style("visibility", "hidden")
             })
+
+        // Adiciona o label inferior de cada barra
         canvas.append("text")
-                .attr("width", 30)
+                .attr("width", larguraBarra)
                 .attr("height", 10)
-                .attr("x", i*35)
-                .attr("y", 165)
+                .attr("x", i * espacoBarra)
+                .attr("y", alturaBarra + 15)
                 .attr("fill", "black")
                 .text( porEstado[i].state )
         
+        // Adiciona o valor numerico de cada barra
         canvas.append("text")
                 .attr("id", "labelestado" + i)
-                .attr("width", 30)
-                .attr("height", 10)
-                .attr("x", i*35)
-                .attr("y", 150-aux*100-2)
+                .attr("x", i * espacoBarra)
+                .attr("y", alturaBarra - aux * 100 - 2)
                 .attr("fill", "black")
                 .text(  Math.floor(porEstado[i].number/1000) )
                 .style("visibility", "hidden")
@@ -193,9 +218,10 @@ function criarEstado(){
 
 csv.then(function(){
 
-///////////////////////////////////////////////////////////// POR ANO
+    // função para gerar gráfico por ano
     criarANO();
-/////////////////////////////////////////////////////   POR ESTADO
+
+    // função para criar grafico por estado
     criarEstado();
     
 
